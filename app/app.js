388c9a6496cdea5e6a99e87631d0abc42eb94cdd
gabe <gabriel.guo@medialand.tw>
2014-04-17 17:11:27 +0800

    成功用scene了 是 function 大小寫錯誤 但在載入資源部分 一直有 error

--- app/app.js
+++ app/app.js
@@ -9,24 +9,31 @@ $(document).ready(function(){
 		init: function(p) {
 			//this._super(p, {asset: 'smurf_sprite.png'});
 			this._super({asset: 'smurf_sprite.png',x:10});
+			//this._super(p,{sheet:'player', frame:7});
+			//this._super(p,{sheet:'player', frame:7});
 		}
 	});
 	
-	/*Q.Scene('scene1', function(stage){
+	//Q.sheet('Player','smurf_sprite.png',{tilew:128, tileh:128});
+	
+	Q.scene('scene1', function(stage){
 		alert('test');
 		var player1= stage.insert(new Q.Player());
 		console.log('x:',player1.p.x);
-	});*/
+	});
 	
-	Q.load('smurf_sprite.png', function(){
+	Q.load(['smurf_sprite.png'], function(){
 		//not working
-		//Q.stageScene('scene1');
+		Q.stageScene('scene1');
+		Q.debug=true;
+		//Q.compileSheets('smurf_sprite.png','sprites.json');
 		
-		var ppl = new Q.Player();
-		Q.gameLoop( function(dt){
-			Q.clear();
-			ppl.update(dt);
-			ppl.render(Q.ctx);
-		});
+		
+		//var ppl = new Q.Player();
+		//Q.gameLoop( function(dt){
+			//Q.clear();
+			//ppl.update(dt);
+			//ppl.render(Q.ctx);
+		//});
 	});
 });
