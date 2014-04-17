3f109f81bcdefb6b8eacbf0b2d28ad6c77241c15
gabe <gabriel.guo@medialand.tw>
2014-04-17 14:54:21 +0800

    setup 不要用串接的方式就沒題 應再測試 jQuery 的 onLoad Q.scene 好像沒起作用 還缺哪裡?

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
-/**
- * ...
- * @author gabe
- */
-$(document).ready(function(){
-  console.log('start');
+window.addEventListener("load", function(){
+	
+	 console.log('start');
 	//error:Uncaught TypeError: Cannot read property 'appendChild' of null
-	var Q = Quintus().include('Sprites, Scenes, Input').setup("canvas").controls().touch();
-	//var Q = Quintus().include('Sprites, Scenes, Input');
+	//var Q = window.Q = Quintus().include("Sprites, Scenes, Input").setup("canvas").controls(true).touch();
+	var Q = Quintus().include('Sprites, Scenes, Input');
+	Q.setup("canvas");
 	
 	Q.Sprite.extend("Player", {
 		init: function(p) {
@@ -14,15 +12,20 @@ $(document).ready(function(){
 		}
 	});
 	
-	Q.Scene('scene1', function(stage){
+	/*Q.Scene('scene1', function(stage){
+		alert('test');
 		var player1= stage.insert(new Q.Player());
 		console.log('x:',player1.p.x);
-		
-		
-	});
-	
+	});*/
 	
 	Q.load('smurf_sprite.png', function(){
-		Q.stageScene('scene1');
+		//Q.stageScene('scene1');
+		
+		var ppl = new Q.Player();
+		Q.gameLoop( function(dt){
+			Q.clear();
+			ppl.update(dt);
+			ppl.render(Q.ctx);
+		});
 	});
 });
